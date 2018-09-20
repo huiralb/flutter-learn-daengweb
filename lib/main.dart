a6ed74e3301301cad02bf1d22e529e0bf2143497
Irnovi Albaweny <huiralb@gmail.com>
2018-09-20 18:14:51 +0700

    part 2: appBar

--- lib/main.dart
+++ lib/main.dart
@@ -2,35 +2,15 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(Homepage());
 
+// part 2
 class Homepage extends StatelessWidget {
   build(context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-          title: Text('Homepage'),
-          leading: Icon(Icons.home),
-          backgroundColor: Colors.green[800]),
-      body: Container(
-          margin: EdgeInsets.all(10.00),
-          child: Column(
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Icon(Icons.archive),
-                  Text('Artikel Terbaru')
-                ],
-              ),
-              Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.network(
-                        'https://flutter.io/images/homepage/header-illustration.png'),
-                    Text('Belajar Flutter')
-                  ],
-                ),
-              ),
-            ],
-          )),
-    ));
+      home: Scaffold(
+          appBar: AppBar(
+              title: Text('Homepage'),
+              leading: Icon(Icons.home),
+              backgroundColor: Colors.green[800])),
+    );
   }
 }
