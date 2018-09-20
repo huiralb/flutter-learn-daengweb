eeaca58de226998c7ff8663e40987c8e16a3ee7a
Irnovi Albaweny <huiralb@gmail.com>
2018-09-20 18:17:20 +0700

    Menampilkan Card, Image & Text di Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,22 @@ class Homepage extends StatelessWidget {
   build(context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-              title: Text('Homepage'),
-              leading: Icon(Icons.home),
-              backgroundColor: Colors.green[800])),
+        appBar: AppBar(
+            title: Text('Homepage'),
+            leading: Icon(Icons.home),
+            backgroundColor: Colors.green[800]),
+        body: Container(
+          child: Card(
+            child: Column(
+              children: <Widget>[
+                Image.network(
+                    'https://flutter.io/images/homepage/header-illustration.png'),
+                Text('Belajar Flutter')
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
